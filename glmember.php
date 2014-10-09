2fa7ec166839fa892d0b0bbee6c3ac70d475fbc4
root <root@dnmaster.dominiosamba.com>
2014-10-08 21:00:58 -0300

    Atualizaçao

--- glmember.php
+++ glmember.php
@@ -2,21 +2,51 @@
 include 'sessao/check.php';
 include 'sessao/connection.php';
 
-$output = ssh2_exec($connection, '/usr/local/samba/bin/samba-tool group listmembers redes');
+
+
+$output = ssh2_exec($connection, '/usr/local/samba/bin/samba-tool group list');
 stream_set_blocking($output, true);
 $cmd = stream_get_contents($output);
 $cmd = explode("\n", $cmd);
+
 ?>
+
+<form action="index.php?page=glmember" method="post">
+<select id="selectBox" onchange="changeFunc();" name="grupo">
+<?foreach ($cmd as $value) {?>
+	<option selected="selected"><?=$value?></option>
+	<?}?>
+
+</select>
+ <input type="submit" value="Listar">
+
+</form>
+
+
+<?php
+	$nomeGrupo = $_POST['grupo'];
+	$output2 =  ssh2_exec($connection, '/usr/local/samba/bin/samba-tool group listmembers'.$nomeGrupo);
+	stream_set_blocking($output2, true);
+	$cmd2 = stream_get_contents($output2);
+	$cmd2 = explode("\n", $cmd2);
+?>
+
 <table>
 		<thead>
 			<tr>
-				<th><h2>Grupo de usuários: </h2></th>
+				<th>Usuarios :</th>
 			</tr>
 		</thead>
 		<tbody>
-		<?foreach ($cmd as $value) {?>
-				<td><?=$value?></td>
+		<?foreach ($cmd2 as $value2) {?>
+				<td><?=$value2?></td>
 			</tr>
 		<?}?>
 		</tbody>
 </table>
+
+
+
+
+
+
--- gmember.php
+++ gmember.php
@@ -2,36 +2,42 @@
 include 'sessao/check.php';
 include 'sessao/connection.php';
 
-//Lista os grupos
+
+
 $output = ssh2_exec($connection, '/usr/local/samba/bin/samba-tool group list');
 stream_set_blocking($output, true);
 $cmd = stream_get_contents($output);
 $cmd = explode("\n", $cmd);
 
-//Lista os usuarios 
-$output2 = ssh2_exec($connection, '/usr/local/samba/bin/samba-tool user list');
-stream_set_blocking($output2, true);
-$cmd2 = stream_get_contents($output2);
-$cmd2 = explode("\n", $cmd2);
-
 ?>
 
-<form action="processo.php?page=gmember" method="POST">
+<form method="post">
+<select id="selectBox" onchange="changeFunc();" name="grupo">
+<?foreach ($cmd as $value) {?>
+	<option selected="selected"><?=$value?></option>
+	<?}?>
 
-	<h4>Grupos</h4>
-		<select style="font-size: 16px;" name="group">
-			<?foreach ($cmd as $value) {?>
-			<option><?=$value?></option>
-			<?}?>
-	</select><br>
-
-	<h4>Usuário</h4>
-		<select style="font-size: 16px;" name="name">
-		<?foreach ($cmd2 as $value2) {?>
-		<option><?=$value2?></option>
-		<?}?>
-	</select><br>
-	<input type="submit" value="Adicionar">
+</select>
+ <input type="submit" value="Listar">
 
 </form>
 
+
+<?php
+	$nomeGrupo = $_POST['grupo'];
+	$output2 =  ssh2_exec($connection, '/usr/local/samba/bin/samba-tool group listmembers'.$nomeGrupo);
+	stream_set_blocking($output2, true);
+	$cmd2 = stream_get_contents($output2);
+	$cmd2 = explode("\n", $cmd2);
+?>
+
+<form action="index.php?page=glmember" method="post">
+<select  name="grupo">
+<?foreach ($cmd2 as $value2) {?>
+	<option selected="selected"><?=$value2?></option>
+	<?}?>
+
+</select><br>
+ <input type="submit" value="Remover">
+
+</form>
